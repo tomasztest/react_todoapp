3c0d015ea791f469952d974ded0dfd6d1363c639
tomasztest <tomaszkotzur@gmail.com>
2019-07-28 15:24:08 +0200

    Updates

---
+++ precache-manifest.911b71acee75516148111d471990a53e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b92753b7c9e5b4048d53f87b5aafe1",
+    "url": "/react_todoapp/index.html"
+  },
+  {
+    "revision": "23bc2a7fa1f33f6b9613",
+    "url": "/react_todoapp/static/css/main.f4bb280f.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/react_todoapp/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "23bc2a7fa1f33f6b9613",
+    "url": "/react_todoapp/static/js/main.ea1a2bcf.chunk.js"
+  },
+  {
+    "revision": "5a6ea62b3820cd1089b1",
+    "url": "/react_todoapp/static/js/runtime~main.62181eeb.js"
+  }
+]);
